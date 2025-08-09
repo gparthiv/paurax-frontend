1b5765ca2c3c5c803a538e0031ab8ed4c1b595b3
Parthiv Ghosh <g.parthiv119@gmail.com>
2025-08-09 12:43:07 +0000

    refactor: Remove text from header link

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,16 +11,15 @@ function Header() {
       {/* PauraX Logo on the left */}
       <img src={logo} alt="PauraX Logo" className="h-10" />
 
-      {/* New WhatsApp Link on the right */}
+      {/* WhatsApp Link on the right (text removed) */}
       <a
         href={whatsappLink}
         target="_blank"
         rel="noopener noreferrer"
-        // Use flexbox to align the text and logo
-        className="flex items-center space-x-2 text-gray-700 font-semibold hover:text-green-600 transition duration-300"
+        className="hover:opacity-80 transition duration-300"
       >
-        <span>Tap to chat with our bot --&gt;</span>
-        <img src={whatsappLogo} alt="WhatsApp" className="h-8 w-8" />
+        {/* The image is now the only content of the link */}
+        <img src={whatsappLogo} alt="Chat on WhatsApp" className="h-10 w-10" />
       </a>
     </header>
   );
